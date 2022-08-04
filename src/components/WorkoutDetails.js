555bcb3cd62e40a927099cccc4d7b1c748ca129f
joycethh <joycethh@outlook.com>
2022-08-04 12:31:20 -0500

    Add delete action type and define the delete function

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,6 +1,24 @@
 import React from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutContext();
+
+  const handleClick = async (e) => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+      // body: JSON.stringify(workout),
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+    });
+
+    const actualData = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: actualData });
+    }
+  };
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -13,6 +31,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.load}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
--- src/context/WorkoutContext.js
+++ src/context/WorkoutContext.js
@@ -3,6 +3,7 @@ import React, { createContext, useReducer } from "react";
 export const WorkoutsContext = createContext();
 
 export const workoutReducer = (state, action) => {
+  console.log(state);
   switch (action.type) {
     case "SET_WORKOUTS":
       return {
@@ -11,6 +12,13 @@ export const workoutReducer = (state, action) => {
     case "CREATE_WORKOUT":
       return {
         workouts: [action.payload, ...state.workouts],
+      };
+    case "DELETE_WORKOUT":
+      console.log(action);
+      return {
+        workouts: state.workouts.filter(
+          (selectedWorkout) => selectedWorkout._id !== action.payload._id
+        ),
       };
     default:
       return state;
