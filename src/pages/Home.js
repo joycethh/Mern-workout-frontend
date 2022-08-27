d1219c25107ea5daa19a9316117a8961c9849a9b
joycethh <joycethh@outlook.com>
2022-08-26 23:04:17 -0500

    make "fetch all workouts data" works

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useWorkoutContext } from "../hooks/useWorkoutsContext";
 
 //components
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
+
 const Home = () => {
-  const { workouts, dispatch } = useWorkoutContext();
+  const { dispatch } = useWorkoutContext();
+  const [workoutData, setWorkoutData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchWorkouts = async () => {
-      const response = await fetch(
-        "https://mern-workout-tracking.herokuapp.com/api/workouts/"
-      );
-      const json = await response.json();
-
-      if (response.ok) {
-        dispatch({ type: "SET_WORKOUTS", payload: json });
-      }
-    };
-
-    fetchWorkouts();
+    setIsLoading(true);
+    fetch("https://mern-workout-tracking.herokuapp.com/api/workouts/")
+      .then(async (response) => {
+        const data = await response.json();
+        setWorkoutData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setWorkoutData([]);
+      });
   }, [dispatch]);
-  return (
+
+  return isLoading ? (
+    <p>Loading Loading Loading Loading</p> // TODO: Add a loading indicator here
+  ) : (
     <div className="home">
       <div className="workouts">
-        {workouts &&
-          workouts.map((workout) => (
-            <WorkoutDetails key={workout._id} workout={workout} />
-          ))}
+        {workoutData.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
       </div>
       <WorkoutForm />
     </div>
