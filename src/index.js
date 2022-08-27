0a4a14d0870cde3f60ce3ee77b474bb173503497
joycethh <joycethh@outlook.com>
2022-08-27 15:29:03 -0500

    modify globe context controller to redux thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
 import "./index.css";
 import App from "./App";
-import { WorkoutsContextProvider } from "./context/WorkoutContext";
+import { reducers } from "./reducer/index";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
