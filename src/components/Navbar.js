0dd2905585dc9116f6d491815ebfd2debb6b27a5
joycethh <joycethh@outlook.com>
2022-09-13 13:25:45 -0500

    add links to the individual auth pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  //user?
+  //JSON.parse(localStorage.getItem(''))
   return (
     <header>
       <div className="container">
         <Link to="/">
-          <h1> Workout Buddy</h1>
+          <h1> Workout Tracking</h1>
         </Link>
+        <nav>
+          <div>
+            <Link to="/login">Login </Link>
+            <Link to="/signup">Sign Up </Link>
+          </div>
+        </nav>
       </div>
     </header>
   );
