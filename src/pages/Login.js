62e901c96683b5a02d9af1495c6f44dcf726edbd
joycethh <joycethh@outlook.com>
2022-09-14 12:00:03 -0500

    use the login hook

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogin } from "../hooks/useLogin";
+
 const Login = () => {
-  const [isSignup, setIsSignup] = useState(false);
+  const { login, error, isLoading } = useLogin();
   const [input, setInput] = useState({
     email: "",
     password: "",
   });
-  const { state, dispatch } = useAuthContext();
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setInput({ ...input, [name]: value });
@@ -14,23 +15,8 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("input", input);
-    // what we do with the input data? ---send to server
-    //1.sign up
-    const loginResponse = await fetch("http://localhost:4000/api/user/login", {
-      method: "POST",
-      body: JSON.stringify(input),
-      // headers: {
-      //   "Content-Type": "application/json",
-      // },
-    });
-
-    //what response we will receive from the server? ----email, token
-    const loginData = await loginResponse.json();
-
-    //
-    dispatch({ type: "SIGNUP", payload: loginData });
-    setIsSignup(true);
+    //pass the input to the login function
+    await login(input);
   };
   return (
     <form className="signup" onSubmit={handleSubmit}>
@@ -39,7 +25,8 @@ const Login = () => {
       <input type="text" name="email" onChange={handleChange} />
       <label>Password</label>
       <input type="text" name="password" onChange={handleChange} />
-      <button type="submit"> Sign In </button>
+      <button disabled={isLoading}> Sign In </button>
+      {error && <div className="error"> {error} </div>}
     </form>
   );
 };
