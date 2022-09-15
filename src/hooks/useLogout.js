736fc23ed2529fcab4d5be8a388db74716d51bc8
joycethh <joycethh@outlook.com>
2022-09-15 14:52:16 -0500

    clear workout global state after user logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,12 +1,15 @@
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useWorkoutContext } from "./useWorkoutsContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: dispatchWorkout } = useWorkoutContext();
   //create logout function
   //user logout --a. to clear local storage b. to update auth context
   const logout = () => {
     localStorage.clear();
     dispatch({ type: "LOGOUT" });
+    dispatchWorkout({ type: "SET_WORKOUTS", payload: null });
   };
 
   return { logout };
